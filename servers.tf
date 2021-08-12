c7600c97bf080f4a2401d918d832457f456dbc92
manideep-personal <88592246+manideep-personal@users.noreply.github.com>
2021-08-12 16:57:38 -0400

    Update servers.tf

--- servers.tf
+++ servers.tf
@@ -2,9 +2,8 @@
 resource "aws_instance" "test-ec2-instance" {
   ami = "${var.ami_id}"
   instance_type = "t2.micro"
-  key_name = "${var.key1}"
+  key_name = "${var.key_name}"
   security_groups = ["${aws_security_group.ingress-all-test.id}"]
-  user_data = "${file("install_nginx.sh")}"
 tags =  {
     Name = "${var.ubuntu}"
   }
