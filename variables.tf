a198e58bbd5a6ea41fceebedeaf822975c63de6b
manideep-personal <88592246+manideep-personal@users.noreply.github.com>
2021-08-12 21:05:52 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 
 variable "region" { default = "us-east-1" }
 variable "instance_type" { default = "t2.micro" }
-variable "key_name" { default = "key" }
+variable "key_name" { default = "github" }
 variable "ami_name"  {default = "ubuntu" }
 variable "ami_id"  {default = "ami-09e67e426f25ce0d7"}
